a62ab7c863f4fcdefcdd7418f0befb3004b454cd
Wojtek Oledzki <wojtek@hoborglabs.com>
2011-11-04 23:30:36 +0100

    removed dashboard styles and added web content

---
+++ htdocs/doc/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$active_link = '/doc';
+$body = file_get_contents(__DIR__ . '/../../includes/doc/index.html');
+
+include_once __DIR__ . '/../../includes/base.phtml';
---
+++ htdocs/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$active_link = '/';
+$body = file_get_contents(__DIR__ . '/../includes/index.html');
+
+include_once __DIR__ . '/../includes/base.phtml';
