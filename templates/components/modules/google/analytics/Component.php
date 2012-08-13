6d48a0212aca42f453846f79de7f1a8d19b40162
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-08-13 10:51:12 +0100

    removing debug code

--- templates/components/modules/google/analytics/Component.php
+++ templates/components/modules/google/analytics/Component.php
@@ -4,6 +4,5 @@ namespace components\modules\google\analytics;
 class Component extends \Hoborg\Bundle\DisplayServiceBundle\Component\Component {
 
 	public function afterLoad() {
-		var_dump('I\'m here :)');
 	}
 }
